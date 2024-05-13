1625b51d4601ee08508a1e8e014dba47eb39a950
zulqarnainnaviwala <48214281+zulqarnainnaviwala@users.noreply.github.com>
2024-05-13 14:55:17 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,38 @@
+let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = "zulqarnain"
+// let score = null
+// let score = undefined
+// let score = true
+
+//let {score} =  req.body // no guarantee if score is coming as integer
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = 1
+// let isLoggedIn = ""
+// let isLoggedIn = "zulqarnain"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "zulqarnain" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
