c88b5093453a862691fa0f9c95b269795125103c
zulqarnainnaviwala <48214281+zulqarnainnaviwala@users.noreply.github.com>
2024-05-13 19:57:35 +0000

    numbers and math object in js

---
+++ 01_basics/06_numbersAndMaths.js
@@ -0,0 +1,43 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4)); //critical to use
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString("en-PK"));
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(4));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random()); //ranges from 0 to 1
+// console.log((Math.random()*10) + 1); //0.
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+// https://www.javatpoint.com/javascript-math
