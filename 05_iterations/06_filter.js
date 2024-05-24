617c44028613530f102e4e12266522b5c810057f
zulqarnainnaviwala <48214281+zulqarnainnaviwala@users.noreply.github.com>
2024-05-24 17:50:57 +0000

    array filter method

---
+++ 05_iterations/06_filter.js
@@ -0,0 +1,42 @@
+//filter in js -> returns a new filtered array so needs to be hold in variable 
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// } )
+
+// const newNums = myNums.filter( (num) =>  num > 4 ) //implicit return
+
+//same above filter task with forEach
+// const newNums = []
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+  //tests
+  let userBooks = books.filter( (book) => book.genre === 'History')
+  console.log(userBooks);
+  userBooks = books.filter( (book) => book.publish >= 2000)
+  console.log(userBooks);
+  userBooks = books.filter( (book) => book.genre !== 'Non-Fiction')
+  console.log(userBooks);
+  userBooks = books.filter( (book) => { 
+    return book.publish >= 1995 && book.genre === "History"
+})
+  console.log(userBooks);
