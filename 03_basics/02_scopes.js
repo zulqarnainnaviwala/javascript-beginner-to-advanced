de22c926870c12e79e0e31bcba57fdce0b45544b
zulqarnainnaviwala <48214281+zulqarnainnaviwala@users.noreply.github.com>
2024-05-24 14:44:18 +0000

    hoisting in javascript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -33,3 +33,20 @@ if (true) {
 }
 // console.log(username);
 
+
+// +++++++++++ Hoisting (can't use function before declaration )+++++++++++
+console.log(addone(5)) // call before function declaration
+//definition function
+function addone(num){
+    return num + 1
+}
+//expression function 
+addTwo(5) //calling function before declaration
+const addTwo = function(num){
+    return num + 2
+}
+
+{} //scope
+// in node env global scope this different as compare to browser
+//test by console.log(this) --> node returns empty obj
+//test by console.log(this) --> browser returns Window obj
