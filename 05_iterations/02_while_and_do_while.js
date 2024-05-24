fee347dd9cf5d11d881357eebe4a604459fbfc6f
zulqarnainnaviwala <48214281+zulqarnainnaviwala@users.noreply.github.com>
2024-05-24 16:16:48 +0000

    while and do-while

---
+++ 05_iterations/02_while_and_do_while.js
@@ -0,0 +1,20 @@
+//while loop
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+let heros = ['flash', "batman", "superman"]
+let arr = 0
+while (arr < heros.length) {
+    //console.log(`Value is ${heros[arr]}`);
+    arr = arr + 1
+}
+
+//do while loop
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
