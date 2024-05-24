1bc8dcf42188bee6bb820e6f74358b8c429c093a
zulqarnainnaviwala <48214281+zulqarnainnaviwala@users.noreply.github.com>
2024-05-24 16:13:27 +0000

    break and continue keywords

--- 05_iterations/01_for_loop.js
+++ 05_iterations/01_for_loop.js
@@ -25,3 +25,20 @@ for (let index = 0; index < myArray.length; index++) {
     
 }
 
+// break and continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//    console.log(`Value of i is ${index}`); 
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+   console.log(`Value of i is ${index}`);  
+}
