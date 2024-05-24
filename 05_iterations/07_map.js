47ee7321b2d8c1eeb2e5b1d9e89527a4ed9674ea
zulqarnainnaviwala <48214281+zulqarnainnaviwala@users.noreply.github.com>
2024-05-24 17:55:13 +0000

    array map method and chaining

---
+++ 05_iterations/07_map.js
@@ -0,0 +1,12 @@
+//map method
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+//chaining concept -> method after method , previous method's return returns automatically pass to next one and so on 
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
